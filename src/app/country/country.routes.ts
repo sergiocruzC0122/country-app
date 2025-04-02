a143fdb655467ff31d10c1e447eeb83e302f8069
sergiocruzC0122 <thiorfthix@gmail.com>
2025-04-02 08:32:48 -0500

    Primera parte del proyecto terminada

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -3,6 +3,7 @@ import { CountryLayoutComponent } from './layouts/CountryLayout/CountryLayout.co
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
 import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
 
 
 export const countryRoutes: Routes = [
@@ -23,6 +24,10 @@ export const countryRoutes: Routes = [
             {
                 path: 'by-region',
                 component: ByRegionPageComponent,
+            },
+            {
+                path: 'by/:code',
+                component: CountryPageComponent,
             },
             {
                 path: '**',
